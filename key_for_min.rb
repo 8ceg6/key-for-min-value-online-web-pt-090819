73a55abb66489fef93fc87251f57748e2b71ff19
cedric e goddard <8ceg6@comcast.net>
2019-10-03 00:05:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,3 +6,19 @@
   
 
 #end
+def key_for_min_value(name_hash)
+  min_value_key=""
+  if name_hash.empty?
+    nil
+  else
+    array=name_hash.to_a
+    min_value=array[0][1]
+    name_hash.each do |key, value|
+    if value < min_value
+      min_value=value
+      min_value_key=key
+    end
+  end
+  min_value_key
+end
+end
