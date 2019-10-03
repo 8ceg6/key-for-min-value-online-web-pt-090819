fcdcd7ee211d2dd1df3daa97b0cda57147b8e9c6
cedric e goddard <8ceg6@comcast.net>
2019-10-03 00:12:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,7 +15,7 @@ def key_for_min_value(name_hash)
     #if the lowest_value is in it's intial state  
     #or if the value of the current key is less than the lowest_value
     #then: set the lowest_value to value and lowest_key to key
-    if lowval == 0 || value < lowest_value
+    if lowval == 0 || value < lowval
       lowval = value 
       low_key = key
     end
